a2b5c8f86f08fde0aaba740734dce155be369996
Joohoon Kim <jayjhkim7@gmail.com>
2021-05-01 15:35:11 -0400

    switch

---
+++ Switch/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        String letter = "B";
+
+        switch(letter.toLowerCase()) {
+            case "a":
+                System.out.println("Letter was " + letter);
+                break;
+            case "b":
+                System.out.println("Letter was " + letter);
+                break;
+            case "c":
+                System.out.println("Letter was " + letter);
+                break;
+            case "d":
+                System.out.println("Letter was " + letter);
+                break;
+            default:
+                System.out.println("No a, b, c, or d");
+        }
+    }
+}
