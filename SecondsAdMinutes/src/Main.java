78feda50070dc6cfbf4adc919beb61a2a1952e98
Joohoon Kim <jayjhkim7@gmail.com>
2021-05-01 10:30:15 -0400

    leading zero

--- SecondsAdMinutes/src/Main.java
+++ SecondsAdMinutes/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(SecondAndMinutes.getDurationString(390, 21));
-//        System.out.println(SecondAndMinutes.getDurationString(10, 21));
-//        System.out.println(SecondAndMinutes.getDurationString(0, -21));
-        System.out.println(SecondAndMinutes.getDurationString(65, 45));
-        System.out.println(SecondAndMinutes.getDurationString(3620));
+        System.out.println(SecondAndMinutes.getDurationString(390, 58));
+        System.out.println(SecondAndMinutes.getDurationString(10, 21));
+        System.out.println(SecondAndMinutes.getDurationString(3000, 121));
+        System.out.println(SecondAndMinutes.getDurationString(65, 5));
+        System.out.println(SecondAndMinutes.getDurationString(307));
         System.out.println(SecondAndMinutes.getDurationString(-110));
+        System.out.println(SecondAndMinutes.getDurationString(65, 45));
+        System.out.println(SecondAndMinutes.getDurationString(3945));
+        System.out.println(SecondAndMinutes.getDurationString(66, 9));
     }
 }
--- SecondsAdMinutes/src/SecondAndMinutes.java
+++ SecondsAdMinutes/src/SecondAndMinutes.java
@@ -3,9 +3,26 @@ public class SecondAndMinutes {
         if(minutes < 0 || seconds < 0 || seconds > 59) {
             return "Invalid value";
         }
+
         int hrs = minutes / 60;
+        String hoursString = hrs + "";
+        if (hrs < 10) {
+            hoursString = "0" + hrs;
+        }
+
         int remainingMinutes = minutes % 60;
-        return (hrs + "h " + remainingMinutes + "m " + seconds + "s ");
+        String minutesString = remainingMinutes + "";
+        if (remainingMinutes < 10) {
+            minutesString = "0" + remainingMinutes;
+        }
+
+        String secondsString = seconds + "";
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        }
+
+
+        return (hoursString + "h " + minutesString + "m " + secondsString + "s ");
     }
 
     public static String getDurationString(int seconds) {
