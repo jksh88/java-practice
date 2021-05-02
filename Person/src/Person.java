ad621cd7cfd41e0392d6a04947d869d42f909543
Joohoon Kim <jayjhkim7@gmail.com>
2021-05-02 19:16:42 -0400

    fixed errors

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -1,30 +1,44 @@
 public class Person {
-    public static String firstName;
-    public static String lastName;
-    public int age;
+    private String firstName;
+    private String lastName;
+    private int age = 0;
 
-    public String getFirstName(){
+    public String getFirstName() {
         return firstName;
     }
-    public String getLastName(){
+
+    public String getLastName() {
         return lastName;
     }
-    public int getAge(){
+
+    public int getAge() {
         return age;
     }
-    public void setFirstName(String name){
+
+    public void setFirstName(String name) {
         this.firstName = name;
     }
-    public void setLastName(String name){
+
+    public void setLastName(String name) {
         this.lastName = name;
     }
-    public void setAge(int age){
-        this.age = age;
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
     }
+
     public boolean isTeen() {
-        return (getAge() > 12 && getAge() <20);
+        return (getAge() > 12 && getAge() < 20);
     }
+
     public String getFullName() {
+        if (firstName == "" || lastName == "") {
+            return firstName + lastName;
+        }
         return firstName + ' ' + lastName;
     }
 }
