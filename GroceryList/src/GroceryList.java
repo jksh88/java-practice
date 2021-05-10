afc27cf08dc91e34917a7b97e426b997d9e444d1
Joohoon Kim <jayjhkim7@gmail.com>
2021-05-10 17:24:47 -0400

    grocery list wip outofrange error

---
+++ GroceryList/src/GroceryList.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGrocery(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in the grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryList(String currentItem, String newItem) {
+        int position = findGroceryItem(currentItem);
+        if(position >= 0) {
+            modifyGroceryList(position, newItem);
+        }
+    }
+
+    private void modifyGroceryList(int position, String newItem) {
+        position = findGroceryItem(newItem);
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified to " + newItem);
+    }
+
+    public void removeGroceryItem(String item) {
+        int position = findGroceryItem(item);
+        if (position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+
+    private void removeGroceryItem(int position) {
+        groceryList.remove(position);
+    }
+
+    public int findGroceryItem(String item) {
+        return groceryList.indexOf(item);
+    }
+
+    public boolean onFile(String searchItem) {
+        int position = findGroceryItem(searchItem);
+        if (position >= 0) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ GroceryList/src/Main.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static GroceryList groceryList = new GroceryList();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        groceryList.printGroceryList();
+
+        boolean quit = false;
+        printInstructions();
+        int choice = 0;
+        while(!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyList();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    findItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0- To print choice options ");
+        System.out.println("\t 1- To print the list of grocery items ");
+        System.out.println("\t 2- To add an item ");
+        System.out.println("\t 3- To modify the list ");
+        System.out.println("\t 4- To remove an item ");
+        System.out.println("\t 5- To find an item ");
+        System.out.println("\t 6- To quit ");
+    }
+
+    public static void addItem() {
+        System.out.println("Type item to add");
+        String item = scanner.nextLine();
+        groceryList.addGrocery(item);
+    }
+
+    public static void modifyList() {
+        System.out.println("Type item to modify");
+        String item = scanner.nextLine();
+        System.out.println("Type new item to add");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryList(item, newItem);
+    }
+
+    public static void removeItem() {
+        System.out.println("Type item to remove");
+        String item = scanner.nextLine();
+        groceryList.removeGroceryItem(item);
+    }
+
+    public static void findItem() {
+        System.out.println("Type item to search for");
+        boolean searchItem = groceryList.onFile(scanner.nextLine());
+        if(!searchItem) {
+            System.out.println("I do not see the item you are looking for");
+        } else {
+            System.out.println("Found " + searchItem + " in the list");
+        }
+    }
+}
