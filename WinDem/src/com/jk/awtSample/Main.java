885473e08360c0d8fdcc2c7456de5fa3c5b9d86c
Joohoon Kim <jayjhkim7@gmail.com>
2021-05-29 15:44:25 -0400

    awt package sample

---
+++ WinDem/src/com/jk/awtSample/Main.java
@@ -0,0 +1,9 @@
+package com.jk.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyWindow myWindow = new MyWindow("Java Saga");
+	    myWindow.setVisible(true);
+    }
+}
