d1a94144fb9ae16d4982b6810316ca769cfd644b
Joohoon Kim <jayjhkim7@gmail.com>
2021-07-02 18:16:22 -0400

    addevendigits complete

--- AddEvenDigits/src/AddEvenDigits.java
+++ AddEvenDigits/src/AddEvenDigits.java
@@ -1,2 +1,18 @@
 public class AddEvenDigits {
+    public static int addEvenDigits(int number) {
+        String numStr = String.valueOf(number);
+        int sum = 0;
+        for(int i = numStr.length(); i >= 0; i--) {
+            System.out.println("number: " + number);
+            int digit = (int)(number % 10);
+            System.out.println("digit: " + digit);
+            if(digit % 2 ==0) {
+                sum += digit;
+            }
+            number /= 10;
+            System.out.println("number: " + number);
+            System.out.println("sum: " + sum);
+        }
+        return sum;
+    }
 }
--- AddEvenDigits/src/Main.java
+++ AddEvenDigits/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        int tri1 = AddEvenDigits.addEvenDigits(43225);
+        System.out.println(tri1);
+    }
 }
