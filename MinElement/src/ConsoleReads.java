371d6f042277737fee293cdc89e071747e416050
Joohoon Kim <jayjhkim7@gmail.com>
2021-07-13 22:40:45 -0400

    minElement complete using arraylist

--- MinElement/src/ConsoleReads.java
+++ MinElement/src/ConsoleReads.java
@@ -2,23 +2,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConsoleReads {
-    //    int[] numArr = {};
     public ArrayList<Integer> numbers = new ArrayList<>();
 
     public ArrayList<Integer> readIntegers(int count) {
         int currentCount = 0;
-        while (currentCount <= count) {
+        while (currentCount < count) {
+            System.out.println("Enter a number: ");
             Scanner input = new Scanner(System.in);
             int num = input.nextInt();
-            numbers.add(num);
             currentCount++;
+            System.out.println(currentCount);
+            numbers.add(num);
         }
         return numbers;
     }
 
     public int findMin(ArrayList<Integer> lst) {
+        System.out.println("lst size: " + lst.size());
         int min = lst.get(0);
-        for (Integer i: lst) {
+        for (int i = 0; i < lst.size(); i++) {
             if (lst.get(i) < min) {
                 min = lst.get(i);
             } else {
--- MinElement/src/Main.java
+++ MinElement/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         ConsoleReads reads = new ConsoleReads();
         ArrayList<Integer> lst = reads.readIntegers(3);
+
         int min = reads.findMin(lst);
-        System.out.println(min);
+        System.out.println("minimum: " + min);
     }
 
 }
