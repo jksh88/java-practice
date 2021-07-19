0577bb5367a1f2dcd8a8c3bfd489d048aaed7106
Joohoon Kim <jayjhkim7@gmail.com>
2021-07-18 20:54:21 -0400

    point complete

--- Point/src/Point.java
+++ Point/src/Point.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Point {
     private int x;
     private int y;
@@ -9,6 +10,43 @@ public class Point {
         this.y = y;
     }
 
+    private int getX() {
+        return x;
+    }
 
+    private int getY() {
+        return y;
+    }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distanceWithoutParams() {
+
+        int sumOfSqrs = x*x + y*y;
+        double result = Math.sqrt(sumOfSqrs);
+        return result;
+    }
+
+    public double distanceWithTwoParams(int a, int b) {
+
+        double result = Math.sqrt((a-x)*(a-x) + (b-y)*(b-y));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        Point point = new Point(x, y);
+        System.out.println(point.distanceWithoutParams());
+        point.setX(x);
+        point.setY(y);
+        double distanceBtwnTwo = point.distanceWithTwoParams(6, 10);
+        System.out.println(distanceBtwnTwo);
+    }
 }
