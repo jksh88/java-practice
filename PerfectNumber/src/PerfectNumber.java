21f533de5540a213236c1360295d755a99e9b49b
Joohoon Kim <jayjhkim7@gmail.com>
2021-07-24 14:42:39 -0400

    optimization attempt

--- PerfectNumber/src/PerfectNumber.java
+++ PerfectNumber/src/PerfectNumber.java
@@ -1,15 +1,19 @@
 public class PerfectNumber {
     private static boolean isPerfectNumber(int num) {
         int sum = 0;
+        int increment = 0;
         if (num < 1) {
             return false;
+        } else {
+            increment = (num % 2 == 0) ? 1: 2;
         }
-        for (int i = 1; i <= num/2; i++ ) {
+
+        for (int i = 1; i <= num/2; i += increment ) {
             if (num % i == 0) {
                 sum += i;
             }
         }
-        return (sum == num) ? true: false;
+        return (sum == num);
     }
 
     public static void main(String[] args) {
