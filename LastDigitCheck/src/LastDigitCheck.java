efe4f953cd345bd2150783cc0828c7634522f3d0
Joohoon Kim <jayjhkim7@gmail.com>
2021-07-23 23:08:41 -0400

    lastDigitCheck

--- LastDigitCheck/src/LastDigitCheck.java
+++ LastDigitCheck/src/LastDigitCheck.java
@@ -1,5 +1,15 @@
 public class LastDigitCheck {
-    private static boolean hasSameLastDigit(int first, int second, int third) {
-        for ()
+    private static boolean hasSameLastDigit(int f, int s, int t) {
+        if ((f < 10 || s < 10 || t < 10 || f > 1000 || s > 1000 || t > 1000)) {
+            return false;
+        } else if ((f % 10) != (s % 10) && (f % 10) != (t % 10) && (s % 10) != (t % 10)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean outcome = hasSameLastDigit(14,44,214);
+        System.out.println(outcome);
     }
 }
