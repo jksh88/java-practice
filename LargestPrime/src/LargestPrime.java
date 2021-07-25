45eb30b1f4821ae99d9a5fdc7d9e2f3da3df467a
Joohoon Kim <jayjhkim7@gmail.com>
2021-07-25 18:06:26 -0400

    largest prime

--- LargestPrime/src/LargestPrime.java
+++ LargestPrime/src/LargestPrime.java
@@ -1,9 +1,8 @@
 // get largest prime factor of a given number;
-
-import java.util.ArrayList;
+//
+//import java.util.ArrayList;
 
 public class LargestPrime {
-    private static ArrayList<Integer> factors = new ArrayList<>();
 
     private static int getLargestPrime(int number) {
         if (number < 0) {
@@ -11,31 +10,23 @@ public class LargestPrime {
         } else if (number == 2) {
             return 2;
         }
-        int maxPrime = 1;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 number /= i;
                 i--;
-                factors.add(i);
-//                System.out.println("i: " + i);
-//                System.out.println(factors.get(i-2));
+                System.out.println("i inner-if scope: " + i + ", number: " + number);
             } else {
+                System.out.println("i outer scope: " + i);
                 continue;
             }
         }
-        factors.forEach(e -> System.out.println(e));
-        for (int j = 0; j < factors.size(); j++) {
-            for (int k = 2; k <= j; k++) {
-                if (factors.get(j) % k == 0) {
-                    factors.remove(j);
-                }
-            }
-        }
-        return factors.get(factors.size() - 1);
+
+        return number;
     }
 
     public static void main(String[] args) {
-        System.out.println(getLargestPrime(10));
+        getLargestPrime(1000);
+//        System.out.println(getLargestPrime(10));
 //        System.out.println(getLargestPrime(217));
 //        System.out.println(getLargestPrime(0));
 //        System.out.println(getLargestPrime(45));
