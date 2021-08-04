fc0205adbab0632a693c6afbfbb1be4277a4b237
Joohoon Kim <jayjhkim7@gmail.com>
2021-08-03 21:17:38 -0400

    AddToArray second method

--- AddToArray/src/SecondWay.java
+++ AddToArray/src/SecondWay.java
@@ -3,12 +3,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SecondWay {
-    public static int[] addUsingArrayList(int len, int[] origArr, int elem) {
+    public static Integer[] addUsingArrayList(Integer[] origArr, int elem) {
 //        List interimArrayList = new ArrayList<>(Arrays.asList(origArr));
-List<Integer> interimArrayList = new ArrayList<Integer>(Arrays.asList(origArr));
-// Q: why does this not work?
+List<Integer> interimArrayList = new ArrayList<>(Arrays.asList(origArr));
 interimArrayList.add(elem);
-int[] newArr = interimArrayList.toArray(origArr);
+Integer[] newArr = interimArrayList.toArray(origArr);
+return newArr;
+    }
 
+    public static void main(String[] args) {
+        Integer[] testArr = new Integer[] {11, 22, 33};
+        Integer[] outputArr = addUsingArrayList(testArr, 88);
+
+        for(Integer e : outputArr) {
+            System.out.println(e);
+        }
     }
 }
