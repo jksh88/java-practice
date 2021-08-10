5fec85221d4c647bf3553989fb780ad372eeb1d4
Joohoon Kim <jayjhkim7@gmail.com>
2021-08-10 19:55:39 -0400

    simple typesafety

--- TypeSafety/src/TypeTest.java
+++ TypeSafety/src/TypeTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TypeTest {
+import java.util.ArrayList;
+
+public class TypeTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(6);
+
+        printDoubled(items);
+    }
+    private static void printDoubled(ArrayList<Integer> n) {
+        for (Integer i : n) {
+            System.out.println(i * 2);
+        }
+    }
 }
