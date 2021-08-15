1965c41a5772a2105350c8a99712f38f9ab8afdd
Joohoon Kim <jayjhkim7@gmail.com>
2021-08-15 12:51:50 -0400

    stream read from a file wip path not working

---
+++ StreamAPI/src/StreamFile.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class StreamFile {
+    public static void main(String[] args) throws IOException {
+        String cwd = Paths.get("").toAbsolutePath().toString();
+        System.out.println(cwd);
+        Stream<String> words = Files.lines(Paths.get("app-requirements-figma-link.txt"));
+        words.forEach(System.out::print);
+        words.close();
+    }
+}
